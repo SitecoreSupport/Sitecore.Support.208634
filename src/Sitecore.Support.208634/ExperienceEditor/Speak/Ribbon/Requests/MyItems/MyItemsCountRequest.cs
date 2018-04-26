47c8e75f332b82f4556fad7d321d0cfb24f3a91b
DK\ala <art.alexeyenko@gmail.com>
2018-04-26 17:57:06 -0400

    Actual patch: index refresh is only launched on lock/unlock operation side effect: MyItems and Workflow/DatasourceUsage requests may block each other - need to be addressed by dev team

--- src/Sitecore.Support.208634/ExperienceEditor/Speak/Ribbon/Requests/MyItems/MyItemsCountRequest.cs
+++ src/Sitecore.Support.208634/ExperienceEditor/Speak/Ribbon/Requests/MyItems/MyItemsCountRequest.cs
@@ -1,30 +1,60 @@
-﻿namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.MyItems
-{
-  using Sitecore.Configuration;
-  using Sitecore.ContentSearch;
-  using Sitecore.ContentSearch.Security;
-  using Sitecore.Data.Items;
-  using Sitecore.Diagnostics;
-  using Sitecore.ExperienceEditor;
-  using Sitecore.ExperienceEditor.Speak.Server.Contexts;
-  using Sitecore.ExperienceEditor.Speak.Server.Requests;
-  using Sitecore.ExperienceEditor.Speak.Server.Responses;
-  using Sitecore.ExperienceEditor.Utils;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Sitecore.Configuration;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Security;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceEditor;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.ExperienceEditor.Utils;
+using Sitecore.Jobs;
 
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.MyItems
+{
+	/// <summary>
+	/// Custom context class: utilizes refreshIndex variable assigned in Lock.js
+	/// </summary>
+  public class ItemContextExtended : ItemContext
+  {
+		[JsonIgnore]
+    private bool _refreshIndex;
+
+    [JsonProperty("refreshIndex")]
+    public bool RefreshIndex
+    {
+      get { return _refreshIndex; }
+      set { _refreshIndex = value; }
+    }
+
+  }
+
+	/// <summary>
+	/// Patched method: makes sure index is only refreshed when item is locked
+	/// </summary>
   public class MyItemsCountRequest : PipelineProcessorRequest<ItemContext>
   {
     public override PipelineProcessorResponseValue ProcessRequest()
     {
       Assert.IsNotNull(base.RequestContext.Database, string.Format("Could not get context.Database for requestArgs:{0}", base.Args.Data));
       Assert.IsNotNull(Factory.GetDatabase(base.RequestContext.Database), string.Format("Could not get database, with name:{0}", base.RequestContext.Database));
-      if (!Sitecore.ExperienceEditor.Settings.WebEdit.ShowNumberOfLockedItemsOnButton)
+
+      ItemContextExtended context = JsonConvert.DeserializeObject<ItemContextExtended>(this.Args.Data);
+
+			if (!Sitecore.ExperienceEditor.Settings.WebEdit.ShowNumberOfLockedItemsOnButton)
       {
         return new PipelineProcessorResponseValue
         {
           Value = string.Empty
         };
       }
-      this.RefreshIndex(base.RequestContext.Item);
+      if (context.RefreshIndex==true)
+      {
+        this.RefreshIndex(base.RequestContext.Item);
+      }
+
       return new PipelineProcessorResponseValue
       {
         Value = ItemUtility.GetLockedItemsCountOfCurrentUser()
@@ -33,11 +63,10 @@
 
     protected virtual void RefreshIndex(Item item)
     {
-      using (IProviderSearchContext providerSearchContext = ContentSearchManager.GetIndex("sitecore_master_index").CreateSearchContext(SearchSecurityOptions.Default))
-      {
-        providerSearchContext.Index.Refresh(new SitecoreIndexableItem(item), IndexingOptions.ForcedIndexing);
-      }
+      var index = ContentSearchManager.GetIndex(new SitecoreIndexableItem(item));
+      index.Refresh(new SitecoreIndexableItem(item), IndexingOptions.ForcedIndexing);
     }
   }
+
 
 }
